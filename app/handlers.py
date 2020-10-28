23a16e79682cc11bce47e2ea691742c116dd7669
Kumud Chauhan <46383902+kumudchauhan@users.noreply.github.com>
2020-10-28 13:22:20 -0400

    Integrated view post workflow with offer help flow
    * Initial MVP commit
    
    * updated keyboard buttons as per https://miro.com/app/board/o9J_kp0wo_Y\=/
    
    * 1. FightPandemics Auth API Integration
    2. Multiple buttons select support for offer help
    3. Updated main menu flow
    4. Code cleanup
    
    * view posts integration in offer help flow
    
    * integrated view posts in offer help flow
    
    * fixed view post keyboard
    
    * fixed view post keyboard and main menu keys

--- app/handlers.py
+++ app/handlers.py
@@ -1,6 +1,8 @@
 from app.keyboards import (start_menu_keyboard, signed_user_menu_keyboard,
-                           unsigned_user_menu_keyboard, help_keyboard, keyboard_checklist)
-from app.fp_api_manager import get_user_posts, get_current_user_profile, login_fp
+                           unsigned_user_menu_keyboard, help_keyboard, keyboard_checklist,
+                           confirm_location_keyboard, get_location_keyboard_markup, share_location_keyboard,
+                           view_posts_keyboard, create_post_keyboard)
+from app.fp_api_manager import get_user_posts, get_current_user_profile, login_fp, get_posts
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
                           CallbackQueryHandler, ConversationHandler)
 
@@ -13,6 +15,11 @@ update.message.reply_text automatically adds the reply only to the specific chat
 """
 
 
+USERNAME_INPUT, PASSWORD_INPUT = map(chr, range(2))
+HELP, LOCATION_CONSENT, LOCATION, SHOWPOST = map(chr, range(2, 6))
+
+
+
 def help_command(update, context):
     """Send a message when the command /help is issued."""
 
@@ -23,11 +30,12 @@ def start(update, context):
     """Send a message to the user when the command /start is issued."""
 
     start_message = "\n".join(['Hi! Welcome!',
-                                         'We are FightPandemics.',
-                                         'A place to offer and request help.',
-                                         'Pandemics will continue to happen.',
-                                         'We help communities prepare and respond.',
-                                         'What would you like to do?'
+
+                               'We are FightPandemics.',
+                               'A place to offer and request help.',
+                               'Pandemics will continue to happen.',
+                               'We help communities prepare and respond.',
+                               'What would you like to do?'
                                ])
     update.message.reply_text(start_message, reply_markup=start_menu_keyboard())
 
@@ -39,13 +47,73 @@ def about(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=fp_about_message)
 
 
-## issue 3 main menu flow
+##### issue 3 main menu flow
 def main_menu(update, context):
     """ Main menu handler: check whether user is logged in or not and display keyboards according to user state"""
 
     # user = update.message.from_user
     if 'logged_in' in context.user_data and context.user_data['logged_in']:
         update.message.reply_text(
+            text='This is FightPandemics Chatbot main menu, What would you like to do?',
+            reply_markup=signed_user_menu_keyboard())
+    else:
+        update.effective_message.reply_text(
+            text='This is FightPandemics Chatbot main menu, What would you like to do?',
+            reply_markup=unsigned_user_menu_keyboard())
+
+
+def view_my_posts(update, context):
+    """ Display the title of post, actual post(limited information), and comments on posts of user"""
+
+    list_to_display = []
+    posts_json = get_user_posts()
+    for idx, post in enumerate(posts_json):
+        list_to_display.append(str(idx + 1) + ". " + post['title'] + "- " +
+                               post['content'] + "- " +
+                               str(post['commentsCount']) + " comments")
+
+    update.effective_message.reply_text(
+        text="\n".join(list_to_display))
+
+
+def view_profile(update, context):
+    """ Display user profile"""
+
+    if "token" in context.user_data:
+        user_info_list = []
+        token = context.user_data["token"]
+        user_profile_json = get_current_user_profile(token=token)
+        print(user_profile_json)
+        user_info_list.append("Name: " + user_profile_json['firstName'] + " " + user_profile_json['lastName'])
+        is_volunteer = "No"
+        if user_profile_json['objectives']['volunteer']:
+            is_volunteer = "Yes"
+
+        user_info_list.append("Volunteer : " + is_volunteer)
+        update.effective_message.reply_text(
+            text="\n".join(user_info_list))
+
+    else:
+        update.effective_message.reply_text("Please login to view your profile")
+
+
+def request_help(update, context):
+    """Send a message when the command /start is issued."""
+
+    # update.message.reply_text('what kind of help do you want?')
+    update.effective_message.reply_text(
+        text="what kind of help do you want?",
+        reply_markup=help_keyboard())
+
+
+########## Issue 5: Login integration using conversation handler ##########
+
+def login_handler():
+    login_conv_handler = ConversationHandler(
+        entry_points=[CallbackQueryHandler(login, pattern='login')],
+
+        states={
+          
             text='This is main menu, What would you like to do?',
             reply_markup=signed_user_menu_keyboard())
     else:
@@ -78,8 +146,8 @@ def login_handler():
 
         name="my_conversation"
     )
-    return conv_handler
-
+    return login_conv_handler
+          
 
 def login(update, context):
     reply_text = "Please provide your username:"
@@ -108,6 +176,102 @@ def password_choice(update, context):
             reply_markup=signed_user_menu_keyboard())
     else:
         update.message.reply_text(
+            text="Incorrect username, password combination. Please try to login again",
+            reply_markup=unsigned_user_menu_keyboard())
+
+
+#### offer help flow #####
+
+
+def offer_help_conv_handler():
+    conv_handler = ConversationHandler(
+        entry_points=[CallbackQueryHandler(offer_help, pattern="offer_help"),
+                      MessageHandler(Filters.text & ~Filters.command, offer_help)],
+        states={
+            HELP: [
+                CallbackQueryHandler(offer_help)
+            ],
+            LOCATION_CONSENT: [
+                CallbackQueryHandler(location)
+            ],
+            LOCATION: [
+                MessageHandler(Filters.location, confirm_location),
+                CallbackQueryHandler(confirm_location)
+            ],
+            SHOWPOST: [
+                CallbackQueryHandler(view_posts)
+            ]
+        },
+        fallbacks=[CommandHandler('start', start)],
+        name="help_handler"
+    )
+    return conv_handler
+
+
+def offer_help(update, context):
+    user_help_keyboard = help_keyboard()
+    update_callback = update.callback_query
+    if update_callback.data != 'done':
+        if 'type' not in context.user_data:
+            context.user_data['type'] = []
+        if update_callback.data == 'offer_help':
+            context.bot.send_message(chat_id=update_callback.message.chat_id,
+                                     text='What type of help would you like to offer?'
+                                          ' Please choose all the relevant tags and click done',
+                                     reply_markup=user_help_keyboard)
+        else:
+            context.user_data['type'].append(update_callback.data)
+            update_callback.edit_message_reply_markup(
+                text='What type of help would you like to offer? Please choose all the relevant tags and click done',
+                reply_markup=keyboard_checklist(user_help_keyboard, context.user_data['type']))
+        return HELP
+
+    else:
+        location_text = 'Please let us know your location so we can show the posts of your area!\n'
+        reply_markup = share_location_keyboard()
+        context.bot.send_message(chat_id=update_callback.message.chat_id, text=location_text, reply_markup=reply_markup)
+        return LOCATION_CONSENT
+
+
+def location(update, context):
+    text_location = "You can share your location by clicking on the send your location button at the bottom!"
+    reply_markup = get_location_keyboard_markup()
+    context.bot.send_message(chat_id=update.callback_query.message.chat_id, text=text_location, reply_markup=reply_markup)
+    return LOCATION
+
+
+def confirm_location(update, context):
+    text = "Your current location is shown above. Do you want to offer help in the same area."
+    reply_markup = confirm_location_keyboard()
+    context.bot.send_message(chat_id=update.message.chat_id, text=text, reply_markup=reply_markup)
+    return SHOWPOST
+
+
+def view_posts(update: object, context):
+    """ Display the relevant posts to the user based on location, type of help.
+    Display title of post, actual post(limited information), and comments on posts of user"""
+    list_to_display = []
+    filter_params_dict = {'type': context.user_data['type']}
+    posts_json = get_posts(filter_params_dict, objective='request')
+    post_categories = ", ".join(context.user_data['type'])
+    for idx, post in enumerate(posts_json):
+        list_to_display.append(str(idx + 1) + ". " + post['title'] + "- " +
+                               post['content'] + "- " +
+                               str(post['commentsCount']) + " comments")
+    if list_to_display:
+        reply_text = "Viewing offer posts for {} help category. \n\n {}".format(
+            post_categories, ", ".join(list_to_display))
+        update.effective_message.reply_text(text=reply_text, reply_markup=view_posts_keyboard())
+
+    else:
+        reply_text = "No post found for {} category. \nPlease create a new post.".format(post_categories)
+        update.effective_message.reply_text(text=reply_text, reply_markup=create_post_keyboard())
+    del context.user_data['type']
+    return ConversationHandler.END
+
+
+def create_post(update, context):
+    if "token" in context.user_data:
             text="Incorrect username, password combination. Please try to login again",reply_markup=unsigned_user_menu_keyboard())
 
 
@@ -194,9 +358,10 @@ def offer_help_conv_handler():
     return conv_handler
 
 
-def create_posts(update,context):
+def create_post(update,context):
     if "token" in context.user_data:
         user_info_list = []
+
         token = context.user_data["token"]
         user_profile_json = get_current_user_profile(token=token)
     else:
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,4 +1,5 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, \
+    ReplyKeyboardMarkup, ReplyKeyboardRemove
 import emoji
 
 
@@ -13,41 +14,99 @@ def start_menu_keyboard():
 
 def help_keyboard():
     keyboard = [[InlineKeyboardButton('Groceries/food', callback_data='Groceries/food'),
-                InlineKeyboardButton('Medical Supplies', callback_data='Medical Supplies'),
-                InlineKeyboardButton('Funding/Donate', callback_data='Funding/Donate')],
+
+                 InlineKeyboardButton('Medical Supplies', callback_data='Medical Supplies'),
+                 InlineKeyboardButton('Funding/Donate', callback_data='Funding/Donate')],
                 [InlineKeyboardButton('Well-being/Mental', callback_data='Well-being/Mental'),
-                InlineKeyboardButton('Information', callback_data='Information'),
-                InlineKeyboardButton('Technology', callback_data='Technology')],
+                 InlineKeyboardButton('Information', callback_data='Information'),
+                 InlineKeyboardButton('Technology', callback_data='Technology')],
                 [InlineKeyboardButton('Legal', callback_data='Legal'),
-                InlineKeyboardButton('Education', callback_data='Education'),
-                InlineKeyboardButton('Business', callback_data='Business')],
+                 InlineKeyboardButton('Education', callback_data='Education'),
+                 InlineKeyboardButton('Business', callback_data='Business')],
                 [InlineKeyboardButton('Done', callback_data='done')]]
     return InlineKeyboardMarkup(keyboard)
 
 
 def signed_user_menu_keyboard():
-    keyboard = [[InlineKeyboardButton('Offer Help', callback_data='offer_help'),
-                InlineKeyboardButton('Request Help', callback_data='request_help')],
+    keyboard = [[InlineKeyboardButton('Request Help', callback_data='request_help'),
+                 InlineKeyboardButton('Offer Help', callback_data='offer_help')],
                 [InlineKeyboardButton('View My Posts', callback_data='view_posts'),
-                InlineKeyboardButton('View My Profile', callback_data='view_profile')],
-                [InlineKeyboardButton('Create posts', callback_data='create_posts'),
-                InlineKeyboardButton('About FightPandemics', callback_data='about')],
+                 InlineKeyboardButton('View My Profile', callback_data='view_profile')],
+                [InlineKeyboardButton('Create Post', callback_data='create_post'),
+                 InlineKeyboardButton('About FightPandemics', callback_data='about')],
                 [InlineKeyboardButton('Signout', callback_data='signout')]]
 
     return InlineKeyboardMarkup(keyboard)
 
 
 def unsigned_user_menu_keyboard():
-    keyboard = [[InlineKeyboardButton('Offer Help', callback_data='offer_help'),
-                InlineKeyboardButton('Request Help', callback_data='request_help')],
+    keyboard = [[InlineKeyboardButton('Request Help', callback_data='request_help'),
+                 InlineKeyboardButton('Offer Help', callback_data='offer_help')],
                 [InlineKeyboardButton('View My Posts', callback_data='view_posts'),
-                InlineKeyboardButton('View My Profile', callback_data='view_profile')],
-                [InlineKeyboardButton('Create posts', callback_data='create_posts'),
-                InlineKeyboardButton('About FightPandemics', callback_data='about')],
+                 InlineKeyboardButton('View My Profile', callback_data='view_profile')],
+                [InlineKeyboardButton('Create Post', callback_data='create_post'),
+                 InlineKeyboardButton('About FightPandemics', callback_data='about')],
+
                 [InlineKeyboardButton('Login', callback_data='login')]]
     return InlineKeyboardMarkup(keyboard)
 
 
+def keyboard_checklist(user_help_keyboard, user_selected_types):
+    outer_length = len(user_help_keyboard.inline_keyboard)
+    for i in range(outer_length):
+        inner_length = len(user_help_keyboard.inline_keyboard[i])
+        for j in range(inner_length):
+            current_button = user_help_keyboard.inline_keyboard[i][j]["text"]
+            if current_button in user_selected_types:
+                user_help_keyboard.inline_keyboard[i][j] = InlineKeyboardButton(
+                    emoji.emojize(current_button + ' :rocket: '),
+                    callback_data=current_button)
+
+    return user_help_keyboard
+
+
+def confirm_location_keyboard():
+    keyboard = [[InlineKeyboardButton('Confirm', callback_data='view_posts'),
+                 InlineKeyboardButton('Type in different Location', callback_data='location')]]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def get_location_keyboard_markup():
+    location_keyboard = KeyboardButton(text="Send Live Location",
+                                       request_location=True)  # creating location button object
+    custom_keyboard = [[location_keyboard]]  # creating keyboard object
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=True)
+    return reply_markup
+
+
+def view_posts_keyboard():
+    keyboard = [[InlineKeyboardButton('1', callback_data='1'),
+                 InlineKeyboardButton('2', callback_data='2'),
+                 InlineKeyboardButton('3', callback_data='3'),
+                 InlineKeyboardButton('4', callback_data='4'),
+                 InlineKeyboardButton('5', callback_data='5')],
+                [InlineKeyboardButton('6', callback_data='6'),
+                 InlineKeyboardButton('7', callback_data='7'),
+                 InlineKeyboardButton('8', callback_data='8'),
+                 InlineKeyboardButton('9', callback_data='9'),
+                 InlineKeyboardButton('10', callback_data='10')],
+                [InlineKeyboardButton('<- Previous', callback_data='previous_page'),
+                 InlineKeyboardButton('Create Post', callback_data='create_post'),
+                 InlineKeyboardButton('Next -> ', callback_data='next_page')]]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def create_post_keyboard():
+    keyboard = [[InlineKeyboardButton('Create Post', callback_data='create_post')]]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def share_location_keyboard():
+    keyboard = [[InlineKeyboardButton('Yes, I would like to share my live location', callback_data='confirm_location')],
+                [InlineKeyboardButton('I will enter my address manually', callback_data='location')]]
+    return InlineKeyboardMarkup(keyboard)
+  
+  
 def keyboard_checklist(help_keyboard, input_data):
     outer_length = len(help_keyboard["inline_keyboard"])
     for i in range(outer_length):
@@ -64,13 +123,5 @@ def keyboard_checklist(help_keyboard, input_data):
                 return help_keyboard
 
     return help_keyboard
-
-
-# can be used later
-# def help_keyboard():
-#     keyboard = [[InlineKeyboardButton('Medical Help', callback_data='medical_help')],
-#                 [InlineKeyboardButton('Other Help', callback_data='other_help')],
-#                 [InlineKeyboardButton('Go to Main Menu', callback_data='main_menu')]]
-#     return InlineKeyboardMarkup(keyboard)
 
 
